f05329c523cd4dca681d969cc2f8463bb00eaa7d
Shyam70233 <deluxshop468@gmail.com>
2022-05-28 09:38:55 +0530

    props-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      
-      
+      <Title />
+      <Conter counter={0} />
+      <Conter counter={1} />
+      <Conter counter={2} />
+
+      <Button>+x</Button>
+      <Button>+y</Button>
+      <Button>+z</Button>
     </div>
   );
+}
+
+function Title() {
+  return <h1>title comp</h1>;
+}
+
+function Conter(props) {
+  return <h2>counter {props.counter}</h2>;
+}
+
+function Button(props) {
+  return <button>{props.children}</button>;
 }
